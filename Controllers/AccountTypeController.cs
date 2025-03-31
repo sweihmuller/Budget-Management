2d8cffc50134cd914fb751a480251491ca822535
Santi <Santi@DESKTOP-IG9DTLS>
2025-03-31 15:17:48 -0300

    Create form for account type

---
+++ Controllers/AccountTypeController.cs
@@ -0,0 +1,24 @@
+﻿using Budget_Management.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Budget_Management.Controllers
+{
+    public class AccountTypeController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(AccountType accountType)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Save the account type to the database}
+                return View(accountType);
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/AccountType.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Budget_Management.Models
+{
+    public class AccountType
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} debe estar lleno.")]
+        [Display(Name = "Nombre")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "La cantidad de caracteres debe ser mayor o igual a {2} o menor o igual a {1}")]
+        public string name { get; set; }
+        [Display(Name = "Nombre de usuario")]
+        public int userId { get; set; }
+        public int order { get; set; }
+
+        public int age{ get; set; }
+        public string email { get; set; }
+        public int creditCard { get; set; }
+    }
+}
