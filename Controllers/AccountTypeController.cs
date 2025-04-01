31ec5ab6f418b5941d37630817df559833a00953
Santi <Santi@DESKTOP-IG9DTLS>
2025-03-31 21:34:41 -0300

    Connection to Database

--- Controllers/AccountTypeController.cs
+++ Controllers/AccountTypeController.cs
@@ -1,13 +1,18 @@
 ﻿using Budget_Management.Models;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace Budget_Management.Controllers
 {
-    public class AccountTypeController : Controller
+    public class AccountTypeController(IConfiguration configuration) : Controller
     {
+        private readonly string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        [HttpGet]
         public IActionResult Create()
         {
-            return View();
+                return View();
         }
 
         [HttpPost]
